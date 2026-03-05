4c304924b5516b5e8418b99939bff12143ca47e0
J-XZ <xuzhenjiang@hhu.edu.cn>
2026-03-05 08:24:04 +0000

    add third_party

---
+++ build.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+set script_dir (dirname (status --current-filename))
+echo "script_dir: $script_dir"
+mkdir -p $script_dir/build
+cd $script_dir/build
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+ninja
