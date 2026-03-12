14b7256b8467d578a8291c20da88868fa3e8b2f9
J-XZ <xuzhenjiang@hhu.edu.cn>
2026-03-12 06:51:53 +0000

    将 CMAKE_BUILD_TYPE 参数从硬编码更改为可配置

--- build.fish
+++ build.fish
@@ -2,7 +2,11 @@
 
 set script_dir (dirname (status --current-filename))
 
+if not set -q CMAKE_BUILD_TYPE
+    set CMAKE_BUILD_TYPE Debug
+end
+
 mkdir -p $script_dir/build
 cd $script_dir/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE ..
 ninja
